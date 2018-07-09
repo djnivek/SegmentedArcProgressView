e76f5c1adfed7428023deae58c16aee9a43610a2
Kevin MACHADO <machadokevin.dev@gmail.com>
2018-07-09 21:43:05 +0200

    Add new protocol for AnimatableViews

---
+++ Sources/AnimatableView.swift
@@ -0,0 +1,14 @@
+//
+//  AnimatableView.swift
+//  SegmentedArcProgressView-iOS
+//
+//  Created by Kévin MACHADO on 12/06/2018.
+//  Copyright © 2018 Zerty Color. All rights reserved.
+//
+
+import Foundation
+
+protocol AnimatableView {
+    func animate()
+    func rewind()
+}
